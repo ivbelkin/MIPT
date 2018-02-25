e0474011904f0be40642cd32eb1ce15d3c3a8d10
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2018-02-25 17:25:11 +0300

    Task 2

--- term_6/industrial_programming/task_2/run.sh
+++ term_6/industrial_programming/task_2/run.sh
@@ -2,4 +2,7 @@
 
 docker build -t ivbelkin/todolist .
 
-docker run -it --rm -v $(pwd)/app:/app -p 8000:8000 ivbelkin/todolist
+docker run --rm -d -v $(pwd)/app:/app -p 8000:8000 --name ivbelkin_todolist ivbelkin/todolist python website/manage.py runserver 0:8000
+docker exec ivbelkin_todolist python website/manage.py makemigrations todolist
+docker exec ivbelkin_todolist python website/manage.py migrate
+docker attach ivbelkin_todolist
